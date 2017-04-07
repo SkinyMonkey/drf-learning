425efc6aec952ea7d20610126a79703cff9cbbb9
adrien candiotti <adrien.candiotti@gmail.com>
2017-04-07 10:22:32 +0800

    mod - cleanup, moved files to subfolders in the api folder

--- whyd/urls.py
+++ whyd/urls.py
@@ -15,37 +15,44 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from rest_framework import routers
-from api import views
-
 from rest_framework_jwt.views import obtain_jwt_token
 
+import api.views.user as user
+import api.views.group as group
+import api.views.playlist as playlist
+import api.views.track as track
+import api.views.follow as follow
+import api.views.like as like
+import api.views.documentation as documentation
+import api.views.stream as stream
+
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-router.register(r'playlists', views.PlaylistViewSet, 'playlists')
-router.register(r'follows', views.FollowViewSet, 'follows')
-router.register(r'likes', views.LikeViewSet, 'likes')
+router.register(r'users', user.UserViewSet)
+router.register(r'groups', group.GroupViewSet)
+router.register(r'playlists', playlist.PlaylistViewSet, 'playlists')
+router.register(r'follows', follow.FollowViewSet, 'follows')
+router.register(r'likes', like.LikeViewSet, 'likes')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^documentation/', views.schema_view),
+    url(r'^documentation/', documentation.schema_view),
 
-    url(r'^me/', views.current_user),
-    url(r'^stream/', views.StreamList.as_view()),
+    url(r'^me/', user.current_user),
+    url(r'^stream/', stream.StreamList.as_view()),
 
     # GET, UPDATE, DELETE tracks
-    url(r'^tracks/(?P<pk>\d+)/', views.TrackView.as_view(), name='track'),
+    url(r'^tracks/(?P<pk>\d+)/', track.TrackView.as_view(), name='track'),
 
     # GET tracks
-    url(r'^tracks/', views.ListTrackView.as_view(), name='tracks'),
+    url(r'^tracks/', track.ListTrackView.as_view(), name='tracks'),
 
     # POST, GET tracks from playlist
-    url(r'^playlists/(?P<playlist_id>\d+)/tracks/', views.ListPlaylistTracksView.as_view(), name='playlist-tracks'),
+    url(r'^playlists/(?P<playlist_id>\d+)/tracks/', playlist.ListPlaylistTracksView.as_view(), name='playlist-tracks'),
 
     # DELETE track from playlist
-    url(r'^playlists/(?P<playlist_id>\d+)/tracks/(?P<track_id>\d+)', views.DeletePlaylistTrackView.as_view(), name='delete-playlist-tracks'),
+    url(r'^playlists/(?P<playlist_id>\d+)/tracks/(?P<track_id>\d+)', playlist.DeletePlaylistTrackView.as_view(), name='delete-playlist-tracks'),
 
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', obtain_jwt_token),

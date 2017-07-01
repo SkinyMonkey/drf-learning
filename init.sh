e638a82e441ae02205d098ecc9438794106aab47
adrien candiotti <adrien.candiotti@gmail.com>
2017-07-01 13:59:24 +0200

    add - cors, init.sh script, start.sh, fixtures

--- init.sh
+++ init.sh
@@ -5,12 +5,8 @@
 virtualenv venv;
 source venv/bin/activate;
 pip install -r requirements.txt;
-mkdir tmp;
+mkdir -p tmp/data;
 python manage.py migrate;
+python manage.py loaddata whyd/fixtures/users.json
 
-echo "The next command will setup the admin user";
-echo "Use adrien for the login and lolilollolilol as the password for the test to work directly";
-echo "Otherwise change the login and password in the tests"
-
-# FIXME : inject the entered login and password into the tests
-python manage.py createsuperuser;
+echo "Login : adrien, Password: agricool"
--- whyd/settings.py
+++ whyd/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -46,6 +47,8 @@ INSTALLED_APPS = [
 #    'social.apps.django_app.default',
 #    'rest_framework_social_oauth2',
     'rest_framework_swagger',
+    
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -96,6 +99,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'whyd.urls'
@@ -178,3 +182,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+APPEND_SLASH = False
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_HEADERS = default_headers + (
+    'default',
+)
